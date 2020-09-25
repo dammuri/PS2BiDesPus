6a7376868b41e99e121b0680eabcf6c40fb1b2f5
amirhsn <amirhusein008@gmail.com>
2020-09-25 13:15:48 +0700

    Nambah autentikasi admin (coba-coba)

--- app/src/main/java/com/example/ps2bidespus/LoginActivity.java
+++ app/src/main/java/com/example/ps2bidespus/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.ps2bidespus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,8 +11,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +30,33 @@ public class LoginActivity extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usr = username.getText().toString();
-                String pwd = password.getText().toString();
-                Intent goHome = new Intent(LoginActivity.this, Home.class);
-                if(usr.equals("")){
-                    Toast.makeText(LoginActivity.this, "Silahkan is username", Toast.LENGTH_SHORT).show();
-                }else if(pwd.equals("")){
-                    Toast.makeText(LoginActivity.this, "silahkan isi password", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(LoginActivity.this, "Hallo Bujank", Toast.LENGTH_SHORT).show();
-                    startActivity(goHome);
-                    finish();
-                }
+                final String usr = username.getText().toString();
+                final String pwd = password.getText().toString();
+                final Intent goHome = new Intent(LoginActivity.this, Home.class);
+
+                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                mDatabase.child("login").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //Get admin
+                        String adminUsr = snapshot.child("admin").child("username").getValue().toString();
+                        String adminPass = snapshot.child("admin").child("password").getValue().toString();
+                        if(usr.equals(adminUsr) && pwd.equals(adminPass)){
+                            Toast.makeText(LoginActivity.this, "Selamat Datang", Toast.LENGTH_SHORT).show();
+                            startActivity(goHome);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, "Username Atau Password Salah", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
     }
